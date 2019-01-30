2e1ea0263c338a67c67b09a9bb7c4b1bfa787ad2
LJXSAM <47037294+LJXSAM@users.noreply.github.com>
2019-01-30 17:39:42 -0500

    Add files via upload

---
+++ guess.py
@@ -0,0 +1,20 @@
+import random
+start = input('请决定随机数字范围开始值')
+end = input('请决定随机数字范围结束值')
+start = int(start)
+end = int(end)
+
+r = random.randint(start,end)
+count = 0 
+while True:
+	count += 1
+	num = input('请猜数字')
+	num = int(num)
+	if num == r:
+		print('你猜对了')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+	 	print('比答案小')
+	print('这是你猜的第', count,'次')
